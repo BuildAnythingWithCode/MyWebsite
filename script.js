9063423c5372fb94223ac015cdeb73f7918e3b63
BuildAnythingWithCode <buildanythingwithcode@gmail.com>
2024-04-28 21:57:39 +0800

    new

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
   var words = ['FUN', 'CREATIVE', 'CHALLENGING', 'PASSIONATE', 'EXCITING', 'DIFFICULT', 'REWARDING', 'ENGAGING', 'INSPIRING', 'EXPRESSION'];
+  var sentence = document.querySelector('.code-is-fun-flex-cont h1');
   var currentWord = document.getElementById('dynamicWord');
-
+  
   currentWord.textContent = words[Math.floor(Math.random() * words.length)];
 
-  currentWord.addEventListener('click', function() {
-      var newWord;
-      do {
-          newWord = words[Math.floor(Math.random() * words.length)];
-      } while (newWord === currentWord.textContent); // Ensure the new word is different
-      currentWord.textContent = newWord;
+  sentence.style.opacity = 1;
+
+  sentence.addEventListener('click', function() {
+      sentence.style.opacity = 0;
+
+      setTimeout(function() {
+          var newWord;
+          do {
+              newWord = words[Math.floor(Math.random() * words.length)];
+          } while (newWord === currentWord.textContent);
+
+          currentWord.textContent = newWord;
+
+          sentence.style.opacity = 1;
+      }, 200);
   });
 });
